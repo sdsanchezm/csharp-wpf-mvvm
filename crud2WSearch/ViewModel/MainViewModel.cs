41cfdf9bb18bebbc5c485b0842dfbd2f1f497229
sdsanchezm <sdsanchezm@gmail.com>
2023-12-13 21:21:14 -0600

    creating the open window feature

--- crud2WSearch/ViewModel/MainViewModel.cs
+++ crud2WSearch/ViewModel/MainViewModel.cs
@@ -1,10 +1,14 @@
-﻿using crud2WSearch.Models;
+﻿using crud2WSearch.Commands;
+using crud2WSearch.Models;
+using crud2WSearch.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace crud2WSearch.ViewModel
@@ -17,10 +21,26 @@ namespace crud2WSearch.ViewModel
         public MainViewModel()
         {
             Users1 = UserManagement.GetUsers();
+            ShowAddUserWindowCommand = new PopupCommand(ShowUserAddWindow, CanShowUserAddWindow);
         }
 
+        private bool CanShowUserAddWindow(object obj)
+        {
+            return true;
+        }
 
-
-
+        private void ShowUserAddWindow(object obj)
+        {
+            // create a generic window
+            var mainWindow = obj as Window;
+            // Create a class from the behind class of the xaml
+            UserAdd userAddWindow = new UserAdd();
+            // set the owner of the add user window
+            userAddWindow.Owner = mainWindow;
+            // set the center of the window
+            userAddWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            // show window
+            userAddWindow.Show();
+        }
     }
 }
--- crud2WSearch/ViewModel/UserAddViewModel.cs
+++ crud2WSearch/ViewModel/UserAddViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using crud2WSearch.Commands;
+using crud2WSearch.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace crud2WSearch.ViewModel
 {
     internal class UserAddViewModel
     {
+        public ICommand AddUserCommand { get; set; }
+        public string? Username { get; set; }
+        public string? Usernumber { get; set; }
+
+        public UserAddViewModel()
+        {
+            AddUserCommand = new PopupCommand(AddUser);
+        }
+
+        private bool CanUserAdd(object obj)
+        {
+            return true;
+        }
+
+        private void AddUser(object obj)
+        {
+            UserManagement.AddUser(new User() { Username = Username, Usernumber = Usernumber });
+        }
+
     }
 }
