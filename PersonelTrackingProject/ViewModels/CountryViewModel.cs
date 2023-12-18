85b55631e18f3bb876621d25f984021bfe8a8fbf
sdsanchezm <sdsanchezm@gmail.com>
2023-12-17 19:17:32 -0600

    user interface adjusted, created a models project and working on Departments

---
+++ PersonelTrackingProject/ViewModels/CountryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonelTracker.Models;
+
+namespace PersonelTrackingProject.ViewModels
+{
+    public class CountryViewModel
+    {
+        public void GetCountry(int id)
+        {
+            var country = new Country
+            {
+                
+            };
+        }
+    }
+}
