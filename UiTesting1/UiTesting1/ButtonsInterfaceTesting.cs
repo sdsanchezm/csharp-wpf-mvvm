4067405af60f25d429df9e49f044023bb0d50c2c
sdsanchezm <sdsanchezm@gmail.com>
2024-02-19 22:40:58 -0600

    adding testing basic structure for ui interfaces HumanInteraction-Like

---
+++ UiTesting1/UiTesting1/ButtonsInterfaceTesting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+
+namespace UiTesting1
+{
+    public class ButtonsInterfaceTesting
+    {
+
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string NotepadAppId = @"C:\progs\AA_REPO_ALL\csharp-wpf-mvvm\wpfMVVM_buttonsInterface\ButtonsInterface\bin\x64\Release\net7.0-windows\ButtonsInterface.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+        protected static WindowsElement editBox;
+
+        public ButtonsInterfaceTesting()
+        {
+            Console.WriteLine("initializing");
+        }
+
+        public WindowsDriver<WindowsElement> TestingProcess()
+        {
+            if (session == null)
+            {
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", NotepadAppId);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+            }
+
+            return session;
+
+        }
+
+        public static void TearDown()
+        {
+            if (session != null)
+            {
+                session.Close();
+
+                try
+                {
+                    session.FindElementByName("Don't Save").Click();
+                }
+                catch { }
+
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
---
+++ UiTesting1/UiTesting1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace UiTesting1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========Starting Test==========");
+
+
+            var desktopSession = new ButtonsInterfaceTesting().TestingProcess();
+
+            // Name of the element from the inspector
+            //desktopSession.FindElementByName("Red").Click();
+            //desktopSession.FindElementByName("+3").Click();
+
+            // AutomationId from the inspector
+            desktopSession.FindElementByAccessibilityId("ButtonRedSection").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            desktopSession.FindElementByAccessibilityId("ButtonGreenSection").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            desktopSession.FindElementByAccessibilityId("ButtonBlueSection").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+
+            desktopSession.Quit();
+            desktopSession = null;
+            //Console.ReadLine();
+        }
+    }
+}
